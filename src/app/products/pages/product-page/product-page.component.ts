30ac250de60f583e7125c15a814ac8c59971d854
cinta <cintatafur@gmail.com>
2024-01-19 12:08:01 +0100

    📚ReactiveFormsModule(Basic steps)

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.scss']
+  styleUrls: ['./product-page.component.scss'],
 })
 export class ProductPageComponent {
+  private formB = inject(FormBuilder);
 
+  public myForm: FormGroup = this.formB.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(6), Validators.email],
+    ],
+    /* region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    borders: ['', [Validators.required]], */
+  });
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
 
-  
+
     ProductPageComponent
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
