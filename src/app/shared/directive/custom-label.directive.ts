f6b8c040b72fd3e5a4349a2d7514d6126d1c6d40
cinta <cintatafur@gmail.com>
2024-01-21 18:18:11 +0100

    📌 Finished(errors)

--- src/app/shared/directive/custom-label.directive.ts
+++ src/app/shared/directive/custom-label.directive.ts
@@ -5,21 +5,26 @@ import {
   Input,
   OnInit,
 } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
 })
 export class CustomLabelDirective implements OnInit {
+  private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
+
   @Input() set color(value: string) {
     this._color = value;
     this.setColor();
   }
-  private htmlElement?: ElementRef<HTMLElement>;
-  private _color: string = 'red';
 
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorsMsg();
+  }
   constructor(private el: ElementRef<HTMLElement>) {
-    /* console.log('cosntructor directiva'); */
-    /*  console.log(el); */
     this.htmlElement = el;
     /*  this.htmlElement.nativeElement.innerHTML = ''; */
   }
@@ -27,12 +32,38 @@ export class CustomLabelDirective implements OnInit {
     this.setColor();
   }
 
-  /*   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight(this.highlightColor || 'yellow');
-  } */
-
-  private setColor() {
+  setColor() {
     if (!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorsMsg(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+    console.log(errors);
+
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+    /*  Validators.minLength(6), Validators.email */
+
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+      this.htmlElement.nativeElement.innerText =
+        `Minimo ${current}/${min} caracteres`;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText = 'No se ajusta al fomato de email';
+      return;
+    }
   }
 }
