8a4141f0d50d52d793c2d78cfdc638764cba0e6f
cinta <cintatafur@gmail.com>
2024-01-22 09:56:15 +0100

    ✅ Make(a signal counter)

--- src/app/signals/pages/counter/counter.component.ts
+++ src/app/signals/pages/counter/counter.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+const name = signal('CinTutuDev')
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent {
+  public counter = signal(10);
+  public squareCounter = computed(() => this.counter() * this.counter());
 
+  constructor(){
+    console.log(name());
+  }
+
+  increaeBy(value: number) {
+    this.counter.update((current) => current + value);
+  }
 }
