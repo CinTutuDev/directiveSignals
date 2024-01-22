93d4e4dc22cc3a9774edb7a64e3fd46df293d02a
cinta <cintatafur@gmail.com>
2024-01-22 12:17:41 +0100

    🔨 error handling

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,6 +1,6 @@
 import { User } from '../../interfaces/user-request.interface';
 import { UserServicesService } from './../../services/user-services.service';
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-user-info-page',
@@ -12,6 +12,10 @@ export class UserInfoPageComponent implements OnInit {
   public userId = signal(1);
   public CurrentUser = signal<User | undefined>(undefined);
   public userWasFound = signal(true);
+  public fullName = computed<string>(() => {
+    if (!this.CurrentUser()) return 'Usuario no encontrado';
+    return `${this.CurrentUser()?.first_name} ${this.CurrentUser()?.last_name}`;
+  });
 
   ngOnInit(): void {
     this.loadUser(this.userId());
@@ -21,10 +25,17 @@ export class UserInfoPageComponent implements OnInit {
     if (id <= 0) return;
 
     this.userId.set(id);
-    this.CurrentUser.set(undefined)
+    this.CurrentUser.set(undefined);
 
-    this.UserServices.getUserById(id).subscribe((user) => {
-      this.CurrentUser.set(user);
+    this.UserServices.getUserById(id).subscribe({
+      next: (user) => {
+        this.CurrentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      error: () => {
+        this.userWasFound.set(false);
+        this.CurrentUser.set(undefined);
+      },
     });
   }
 }
