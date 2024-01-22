497f68c4758466959fa7ec441bd7dd10df5f590b
cinta <cintatafur@gmail.com>
2024-01-22 11:22:18 +0100

    💚 show (users)

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
+import { UserServicesService } from './../../services/user-services.service';
+import { Component, OnInit, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
-  styleUrls: ['./user-info-page.component.scss']
+  styleUrls: ['./user-info-page.component.scss'],
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  private UserServices = inject(UserServicesService);
+  public userId = signal(1);
+  public CurrentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
 
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number) {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.CurrentUser.set(undefined)
+
+    this.UserServices.getUserById(id).subscribe((user) => {
+      this.CurrentUser.set(user);
+    });
+  }
 }
